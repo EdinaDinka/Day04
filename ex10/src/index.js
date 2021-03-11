62bfd723a77d04723ddde63036b2ee6d4a9f81ec
EdinaDinka <cuturaedina@gmail.com>
2021-03-11 21:13:04 +0100

    Added ex11

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+
+    var myFirstName = "Edina";
+    var myLastName = "Sadiković";
+    var myHomeTown = "Sarajevo";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+
+}
+console.log(main());
+module.exports = main;
