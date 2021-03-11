004b4d672f5af6c7e0a47afe2d4d40b8d4349fc4
EdinaDinka <cuturaedina@gmail.com>
2021-03-11 17:13:24 +0100

    Added ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+function main (numOne, numTwo){
+
+    var numOne;
+    var numTwo;
+    var myQuotient = numOne / numTwo;
+    return myQuotient;
+
+}
+console.log(main(parseFloat(5.0), parseFloat(2.0)).toFixed(1));
+console.log(main(parseFloat(16.5), parseFloat(5.5)).toFixed(2));
+console.log(main(parseFloat(102.0), parseFloat(25.5)).toFixed(1));
+module.exports = main;
