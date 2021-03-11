abd68ce7a0758a5e7c2c411692dd4b4bb54759c2
EdinaDinka <cuturaedina@gmail.com>
2021-03-11 17:04:05 +0100

    Added ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+function main (numOne, numTwo){
+
+    var numOne;
+    var numTwo;
+    var product = numOne * numTwo;
+    return product;
+
+}
+console.log(main(parseFloat(2.5), parseFloat(2.0)).toFixed(1));
+console.log(main(parseFloat(5.5), parseFloat(5.5)).toFixed(2));
+console.log(main(parseFloat(100.25), parseFloat(43.2)).toFixed(1));
+module.exports = main;
